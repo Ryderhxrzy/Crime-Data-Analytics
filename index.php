befe98e81d0ba64eb10dd75252a9fbcae87c7ce8
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2025-12-11 23:37:13 +0800

    change title

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - Crime Data Analytics</title>
+    <title>Login - AlerTaraQC</title>
     <link rel="stylesheet" href="frontend/css/global.css">
     <link rel="stylesheet" href="frontend/css/login.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
